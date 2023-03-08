5a935498ccd581a8c9c3a6f576d41f28266893f3
Patryk <patryktofil03@gmail.com>
2023-03-08 09:54:32 +0100

    init for portfolio

---
+++ src/components/topbar/Topbar.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import "./topbar.scss";
+
+
+export default function Topbar() {
+  return (
+    <div className="topbar">
+      <div className="top">
+        <div className="top-left">
+          <a href="https://github.com/PTFOPlayer/technews"><i className="topIcon fab fa-github"></i></a>
+        </div>
+        <div className="top-center">
+          <ul className="top-list">
+            <li className="top-list-item"><a href='google.com' className="inherit">Home</a></li>
+            <li className="top-list-item"><a href='google.com' className="inherit">Projects</a></li>
+            <li className="top-list-item"><a href='google.com' className="inherit">Contacts</a></li>
+          </ul>
+        </div>
+        <div className="top-right" />
+      </div>
+    </div>
+  );
+}
