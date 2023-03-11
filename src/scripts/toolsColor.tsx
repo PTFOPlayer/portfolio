e02bfba7e71461f644e5bed5967d05a8ffe5c480
Patryk <patryktofil03@gmail.com>
2023-03-11 22:11:04 +0100

    fixed tools

--- src/scripts/toolsColor.tsx
+++ src/scripts/toolsColor.tsx
@@ -6,12 +6,15 @@ class toolColor {
       <>
         {
             tool === "rust" || tool === "Rust" ? <p className="rust"><i className='fab fa-rust' /> {tool}</p> :
-            tool === "mongodb" || tool === "Mongodb" ? <p className="mongodb"><i className="fas fa-database" />MongoDB</p> :
+            tool === "mongodb" || tool === "Mongodb" || tool === "MongoDB" ? <p className="mongodb"><i className="fas fa-database" />MongoDB</p> :
             tool === "c++" || tool === "C++" ? <p className="cpp"> C++ </p> :
-            tool === "shell" ? <p className="shell"><i className="fa-solid fa-terminal" /> shell </p> :
-            tool === "docker" ? <p className="docker"><i className="fa-brands fa-docker" /> docker </p> :
+            tool === "shell" ? <p className="shell"><i className="fa-solid fa-terminal" /> Shell </p> :
+            tool === "docker" ? <p className="docker"><i className="fa-brands fa-docker" /> Docker </p> :
+            tool === "python" || tool === "Python"  ? <p className="python"><i className="fa-brands fa-python" /> Python </p> :
             tool === "Javascript" || tool === "javascript" || tool === "js" || tool === "JS" ? <p className="js"><i className="fa-brands fa-js" /> Javascript </p> :
             tool === "Typescript" || tool === "typescript" || tool === "ts" || tool === "TS" ? <p className="ts"> Typescript </p> : 
+            tool === "scss" || tool === "SCSS" || tool === "Scss" ? <p className="sass"><i className="fa-brands fa-sass" /> Scss </p> :
+            tool === "React" || tool === "react" ? <p className="react"><i className="fa-brands fa-react" /> React </p> :
             <p>{tool}</p>
         }
     </>
