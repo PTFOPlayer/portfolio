796b16593b63072a0a8044f5d0ca88e63804a314
PTFOPlayer <patryk041103@gmail.com>
2023-03-11 22:55:48 +0100

    fixed some stuid if statement

--- src/scripts/toolsColor.tsx
+++ src/scripts/toolsColor.tsx
@@ -2,29 +2,44 @@ import React from "react";
 
 class toolColor {
   public static tool(tool: String) {
-    return (
-      <>
-        {
-            tool === "rust" || tool === "Rust" ? <p className="rust"><i className='fab fa-rust' /> {tool}</p> :
-            tool === "mongodb" || tool === "Mongodb" || tool === "MongoDB" ? <p className="mongodb"><i className="fas fa-database" />MongoDB</p> :
-            tool === "c++" || tool === "C++" ? <p className="cpp"> C++ </p> :
-            tool === "shell" ? <p className="shell"><i className="fa-solid fa-terminal" /> Shell </p> :
-            tool === "docker" ? <p className="docker"><i className="fa-brands fa-docker" /> Docker </p> :
-            tool === "python" || tool === "Python"  ? <p className="python"><i className="fa-brands fa-python" /> Python </p> :
-            tool === "Javascript" || tool === "javascript" || tool === "js" || tool === "JS" ? <p className="js"><i className="fa-brands fa-js" /> Javascript </p> :
-            tool === "Typescript" || tool === "typescript" || tool === "ts" || tool === "TS" ? <p className="ts"> Typescript </p> : 
-            tool === "scss" || tool === "SCSS" || tool === "Scss" ? <p className="sass"><i className="fa-brands fa-sass" /> Scss </p> :
-            tool === "React" || tool === "react" ? <p className="react"><i className="fa-brands fa-react" /> React </p> :
-            <p>{tool}</p>
-        }
-    </>
-    ) 
+
+    tool = tool.toLowerCase()
+
+    let toolSwitch = (t: String) => {
+      switch (t) {
+        case "rust": 
+          return <p className="rust"><i className='fab fa-rust' /> Rust </p>
+        case "mongodb": 
+          return <p className="mongodb"><i className="fas fa-database" />MongoDB</p> 
+        case "c++":
+          return <p className="cpp"> C++ </p>
+        case "shell":
+          return <p className="shell"><i className="fa-solid fa-terminal" /> Shell </p>
+        case "docker":
+          return <p className="docker"><i className="fa-brands fa-docker" /> Docker </p>
+        case "python":
+          return <p className="python"><i className="fa-brands fa-python" /> Python </p>
+        case "javascript":
+          return <p className="js"><i className="fa-brands fa-js" /> Javascript </p> 
+        case "typescript":
+          return <p className="ts"> Typescript </p>
+        case "scss":
+          return <p className="sass"><i className="fa-brands fa-sass" /> Scss </p>
+        case "react":
+          return <p className="react"><i className="fa-brands fa-react" /> React </p>
+        default:
+          return <p>{t}</p>
+      }
+    }
+
+    return (<>{toolSwitch(tool)}</>)
   }
+
   public static tools(tools:Array<String>) {
     return (
       <>
-        {tools.map(t => (
-          <li>
+        {tools.map((t, key: number) => (
+          <li key={key}>
             {this.tool(t)}
           </li>
         ))}
