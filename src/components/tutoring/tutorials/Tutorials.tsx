e551eb1c296be64a76cb1995e72b92d497774776
PTFOPlayer <patryk041103@gmail.com>
2023-05-24 14:20:40 +0200

    moved some functions

---
+++ src/components/tutoring/tutorials/Tutorials.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+export default function Tutorials() {
+  return (
+    <>
+      <h1>Only in polish!</h1>
+      <h2> kursy </h2>
+      <ul className="courses_list">
+        <li><a href='/#/tutoring' className="inherit">Cooming soon</a></li>
+      </ul>
+    </>
+  )
+}
