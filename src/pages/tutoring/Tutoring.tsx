da84d4ea41967e203497dee2fd9da9441c77abfe
Patryk <patryktofil03@gmail.com>
2023-05-24 14:20:40 +0200

    moved some functions

--- src/pages/tutoring/Tutoring.tsx
+++ src/pages/tutoring/Tutoring.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import "./tutoring.scss"
 import Offer from "../../components/tutoring/offer/Offer";
+import Tutorials from "../../components/tutoring/tutorials/Tutorials";
 export default function Tutoring() {
   return (
     <div className="tutoring">
-      <h1>Only in polish!</h1>
-      <h2> kursy </h2>
-
-      <ul className="courses_list">
-        <li><a href='/#/tutoring' className="inherit">Cooming soon</a></li>
-      </ul>
-
+      <Tutorials />
       <Offer />
     </div>
   )
