8940cb3a0902b6aad0e82530b8ae30ae1fdd5cb2
PTFOPlayer <patryk041103@gmail.com>
2023-09-05 19:16:39 +0200

    basic code highlight

--- src/components/tutorial/Tutorial.tsx
+++ src/components/tutorial/Tutorial.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import './tutorial.scss';
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'
+
+
 export function Title(props: { children: string }) {
   return (
-      <h1 className="tutorialh1">{props.children}</h1>
+    <h1 className="tutorialh1">{props.children}</h1>
   )
 }
 
 export function HeadlineBig(props: { children: string }) {
   return (
-      <h2 className="tutorialh2">{props.children}</h2>
+    <h2 className="tutorialh2">{props.children}</h2>
   )
 }
 
@@ -20,14 +24,14 @@ export function HeadlineSmall(props: { children: string }) {
 
 export function Paragraph(props: { children: string }) {
   return (
-      <p className="tutorialp">{props.children}</p>
+    <p className="tutorialp">{props.children}</p>
   )
 }
 
 export function Note(props: { children: string }) {
   return (
-    <p className="tutorialnote">{props.children}</p>
-  ) 
+    <p className="tutorialnote">{"-> " + props.children}</p>
+  )
 }
 
 export function Table(props: { table: {} }) {
@@ -36,4 +40,33 @@ export function Table(props: { table: {} }) {
 
 export function List(props: { list: {} }) {
   return (<></>)
+}
+
+interface code_from_str {
+  lang: string,
+  code: string
+}
+
+export function CodeBlock(props: { children: string }) {
+  let data: code_from_str;
+  try {
+
+    data = JSON.parse(props.children) as code_from_str;
+  } catch {
+    data = { lang: "js", code: "lorem" };
+  }
+  let text = data.code.split("\n");
+
+  console.log(data.code)
+  return (<div className="codeblock">
+    <h3>{data.lang}</h3>
+    <div className="code">
+      {
+        text.map((e) => {
+          const code = Prism.highlight(e, Prism.languages.js, data.lang);
+          return (<div dangerouslySetInnerHTML={{ __html: code }}></div>)
+        })
+      }
+    </div>
+  </div>)
 }
