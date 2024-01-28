5809a9f0a5300a2d290ecc1ade6e1cfbde1587e9
PTFOPlayer <patryk041103@gmail.com>
2024-01-28 17:22:46 +0100

    more styling changes, code simplification

--- src/components/home/os/Os.tsx
+++ src/components/home/os/Os.tsx
@@ -67,12 +67,12 @@ export default function Os() {
         </p>
         <br />
         <p>
-          Going deeper into OS choice, why <span className="fedora">Fedora</span>? Because I wanted something
-          that had a high level of hardware compatibility and a large package
-          repository. DNF has a lot of packages available, is fast and Delta RPM
-          saves some download time when doing large updates. I ditched nvidia
-          GPU's entierly so now I can fully enjoy modern wayland compositors
-          that Fedora offers by default.
+          Going deeper into OS choice, why <p className="fedora">Fedora</p>?
+          Because I wanted something that had a high level of hardware
+          compatibility and a large package repository. DNF has a lot of
+          packages available, is fast and Delta RPM saves some download time
+          when doing large updates. I ditched nvidia GPU's entierly so now I can
+          fully enjoy modern wayland compositors that Fedora offers by default.
         </p>
       </div>
     </>
--- src/components/projects/post/Post.tsx
+++ src/components/projects/post/Post.tsx
@@ -1,52 +1,72 @@
 import { motion, useAnimation } from "framer-motion";
 import { useInView } from "react-intersection-observer";
-import React from "react"
-import toolsColor from "../../../scripts/toolsColor"
-import "./post.scss"
+import React from "react";
+import toolsColor from "../../../scripts/toolsColor";
+import "./post.scss";
 export default function Post(element: any) {
-  const control = useAnimation()
-  const [ref, inView] = useInView()
+  const control = useAnimation();
+  const [ref, inView] = useInView();
   const variants = {
-    visible: {opacity: 1, scale: 1, transition: {duration: 0.7} },
-    hidden: {opacity: 0, scale: 0,},
-  }
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.7 } },
+    hidden: { opacity: 0, scale: 0 },
+  };
 
   React.useEffect(() => {
     if (inView) {
       control.start("visible");
     } else {
-      control.start("hidden")
+      control.start("hidden");
     }
   }, [control, inView]);
-    return (
+  return (
     <>
-        <div className="post" >
-          {element.image ? 
-            <motion.img src={element.img} alt="img not found" loading="lazy" 
-            ref={ref} variants={variants} initial="hidden" animate={control}/> :
-            <motion.img src="https://avatars.githubusercontent.com/u/35374730?v=4" alt="img not found" loading="lazy"
-            ref={ref} variants={variants} initial="hidden" animate={control}/>
-          }
-          <div className="content">
-            <div className="titleBar">
-              {element.title ? <h1>{element.title}</h1> : null}
-              {element.tools ?
-                <motion.ul
-                ref={ref} variants={variants} initial="hidden" animate={control}>
-                  {toolsColor.tools(element.tools)}
-                </motion.ul> : null
-              }
-            </div>
-            <div className="projectDescription">
-              {element.description ? 
+      <div className="post">
+        {element.image ? (
+          <motion.img
+            src={element.img}
+            alt="img not found"
+            loading="lazy"
+            ref={ref}
+            variants={variants}
+            initial="hidden"
+            animate={control}
+          />
+        ) : (
+          <motion.img
+            src="https://avatars.githubusercontent.com/u/35374730?v=4"
+            alt="img not found"
+            loading="lazy"
+            ref={ref}
+            variants={variants}
+            initial="hidden"
+            animate={control}
+          />
+        )}
+        <div className="content">
+          <div className="titleBar">
+            {element.title ? <h1>{element.title}</h1> : null}
+            {element.tools ? (
+              <motion.ul
+                ref={ref}
+                variants={variants}
+                initial="hidden"
+                animate={control}
+              >
+                {toolsColor.tools(element.tools)}
+              </motion.ul>
+            ) : null}
+          </div>
+          <div className="projectDescription">
+            {element.description ? (
               <>
                 <h3>Description:</h3>
                 <p>{element.description}</p>
-              </>:
-              null}
-              {element.link ? <a href={element.link}>{element.link}</a> : null}
-            </div>
+              </>
+            ) : null}
+            {element.link ? <a href={element.link}>{element.link}</a> : null}
           </div>
         </div>
-    </>)
+      </div>
+    </>
+  );
 }
