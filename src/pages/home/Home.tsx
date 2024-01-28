71606da48dad65fde6f64ddac05d1936a8d99974
Patryk <patryktofil03@gmail.com>
2024-01-28 13:47:02 +0100

    fixed some issues remade home page UI

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,16 +2,14 @@ import * as React from "react";
 import "./home.scss";
 
 import Description from "../../components/home/description/Description";
-import PostsList from "../../components/home/postsList/PostsList";
 import Toolset from "../../components/home/toolset/Toolset";
 import Os from "../../components/home/os/Os";
 export default function Home() {
   return (
     <div className="home">
-      <Description/>
-      <PostsList/>
-      <Toolset/>
-      <Os/>
+      <Description />
+      <Toolset />
+      <Os />
     </div>
   );
 }
