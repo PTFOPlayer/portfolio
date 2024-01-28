4a14be81485ee78a30f695945f451a1e1ebaa3ed
Patryk <patryktofil03@gmail.com>
2024-01-28 17:22:46 +0100

    more styling changes, code simplification

--- src/components/home/description/Description.tsx
+++ src/components/home/description/Description.tsx
@@ -1,29 +1,27 @@
 import * as React from "react";
-import "./description.scss"
-import "../../../assets/langs.scss"
+import "./description.scss";
+import "../../../assets/langs.scss";
 
 export default function Description() {
-  const [isReactSpinning, setReactSpin] = React.useState(false);
-  const [isRustSpinning, setRustSpin] = React.useState(false);
-  const [isTuxJumping, setTuxJumping] = React.useState(false);
   const [isWindowsGlitch, setWindowsGlitch] = React.useState(false);
 
   return (
     <div className="description">
       <h1> Hi, I'm Patryk </h1>
-      <img src="https://avatars.githubusercontent.com/u/35374730?v=4" alt="" loading="lazy"/>
-      <p> I'm
-        <span className="dev"> full-stack </span>
+      <img
+        src="https://avatars.githubusercontent.com/u/35374730?v=4"
+        alt=""
+        loading="lazy"
+      />
+      <p>
+        {" "}
+        I'm
+        <p className="dev"> full-stack </p>
         developer from Poland.
         <br /> <br />
         I'm using
-        <p className="react"
-          onMouseEnter={() => { setReactSpin(true) }}
-          onMouseLeave={() => { setReactSpin(false) }}>
-          {isReactSpinning ?
-            <i className="fa-brands fa-react spin" /> :
-            <i className="fa-brands fa-react" />
-          }
+        <p className="react">
+          <i className="fa-brands fa-react" />
           React
         </p>
         and
@@ -37,60 +35,55 @@ export default function Description() {
           MongoDB
         </p>
         . I can program in
-        <p className="rust"
-          onMouseEnter={() => { setRustSpin(true) }}
-          onMouseLeave={() => { setRustSpin(false) }}>
-          {isRustSpinning ?
-            <i className='fab fa-rust spin' /> :
-            <i className='fab fa-rust' />
-          }
+        <p className="rust">
+          <i className="fab fa-rust" />
           Rust
-        </p>,
-        <p className="cpp">
-          C++
-        </p>,
+        </p>
+        ,<p className="cpp">C++</p>,
         <p className="js">
           <i className="fa-brands fa-js" />
           Javascript
-        </p>,
-        <p className="ts">
-          Typescript
         </p>
+        ,<p className="ts">Typescript</p>
         and
         <p className="python">
           <i className="fa-brands fa-python" />
           Python
-        </p>.
+        </p>
+        .
         <br /> <br />
         I'm using
-        <p className="fedora">
-          Fedora
-        </p>
+        <p className="fedora">Fedora</p>
         as my main operating system and I generaly prefer
-        <p className="linux"
-          onMouseEnter={() => { setTuxJumping(true) }}
-          onMouseLeave={() => { setTuxJumping(false) }}>
-          {isTuxJumping ?
-            <i className="fab fa-linux jump" /> :
-            <i className="fab fa-linux" />
-          }
+        <p className="linux">
+          <i className="fab fa-linux" />
           Linux
         </p>
         based systems over
-        <p className="windows"
-          onMouseEnter={() => { setWindowsGlitch(true) }}
-          onMouseLeave={() => { setWindowsGlitch(false) }}>
-          {isWindowsGlitch ?
+        <p
+          className="windows"
+          onMouseEnter={() => {
+            setWindowsGlitch(true);
+          }}
+          onMouseLeave={() => {
+            setWindowsGlitch(false);
+          }}
+        >
+          {isWindowsGlitch ? (
             <>
               <i className="fa-brands fa-windows glitch" />
               <span className="glitch">Windows</span>
-            </> :
+            </>
+          ) : (
             <>
               <i className="fa-brands fa-windows" />
               <>Windows</>
-            </>}
+            </>
+          )}
         </p>
-        , but I have no problem with using it too, in fact I'm using it for gaming.
+        , but I have no problem with using it too, in fact I'm using it for
+        gaming.
       </p>
-    </div>);
+    </div>
+  );
 }
