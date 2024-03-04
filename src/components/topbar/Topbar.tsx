75c54c5a8b1b7146c02332a89cd93dc23b59464d
PTFOPlayer <patryk041103@gmail.com>
2024-03-04 20:35:15 +0100

    huge ui change, remade topbar

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -1,84 +1,114 @@
 import * as React from "react";
 import "./topbar.scss";
-import { UserAgentProvider, UserAgent } from "@quentin-sommer/react-useragent";
+import { useState } from "react";
+
+interface Item {
+  name: string;
+  url: string;
+}
 
 export default function Topbar() {
+  let list: Array<Item> = [
+    { url: "/#/", name: "Home" },
+    { url: "/#/projects", name: "Projects" },
+    { url: "/#/career", name: "Career" },
+    { url: "/#/contact", name: "Contact" },
+    { url: "/#/tutorials", name: "Tutorials" },
+  ];
+
+  const width = window.innerWidth;
+
+  if (width < 1024) {
+    return <MobileTopbar list={list} />;
+  } else {
+    return <DestkopTopbar list={list} />;
+  }
+}
+
+function MobileTopbar(props: { list: Array<Item> }) {
+  const [display, setDisplay] = useState("hidden");
   return (
-    <UserAgentProvider ua={window.navigator.userAgent}>
-      <div className="topbar">
-        <div className="top">
-          <div className="top-left">
-            <UserAgent mobile>
-              <a className="bmc" href="https://www.buymeacoffee.com/WhiskyAKM">
-                <img
-                  src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-                  alt="Buy Me A Coffee"
-                />
-              </a>
-            </UserAgent>
-            <a href="https://github.com/PTFOPlayer/">
-              <i className="fab fa-github"></i>
-            </a>
-          </div>
-          <div className="top-center">
-            <ul className="top-list">
-              <li className="top-list-item">
-                <a href="/#/" className="inherit">
-                  Home
-                </a>
-              </li>
-              <li className="top-list-item">
-                <a href="/#/projects" className="inherit">
-                  Projects
-                </a>
-              </li>
-              <li className="top-list-item">
-                <a href="/#/contact" className="inherit">
-                  Contact
-                </a>
-              </li>
-              <li className="top-list-item">
-                <a href="/#/tutorials" className="inherit">
-                  Tutorials
-                </a>
-              </li>
-              <UserAgent mobile>
-                <li className="top-list-item">
-                  <a
-                    className="top-list-item"
-                    href="https://ptfoplayer.github.io/technews/#/"
-                  >
-                    Technews
-                  </a>
-                </li>
-              </UserAgent>
-            </ul>
-          </div>
-          <div className="top-right">
-            <ul className="top-list">
-              <UserAgent computer>
-                <a
-                  className="bmc"
-                  href="https://www.buymeacoffee.com/WhiskyAKM"
-                >
-                  <img
-                    src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-                    alt="Buy Me A Coffee"
-                  />
-                </a>
-                <li className="top-list-item">
-                  <a
-                    className="top-list-item"
-                    href="https://ptfoplayer.github.io/technews/#/"
-                  >
-                    Technews
-                  </a>
-                </li>
-              </UserAgent>
-            </ul>
-          </div>
+    <>
+      <div className="top">
+        <div className="top-left"></div>
+        <div className="top-center">
+          <a href="https://github.com/PTFOPlayer/">
+            <i className="fab fa-github"></i>
+          </a>
+          <a className="bmc" href="https://www.buymeacoffee.com/WhiskyAKM">
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+              alt="Buy Me A Coffee"
+            />
+          </a>
+        </div>
+        <div className="top-right">
+          <button
+            onClick={() => {
+              display === "top-list"
+                ? setDisplay("hidden")
+                : setDisplay("top-list");
+            }}
+          >
+            <i className="fa-solid fa-bars dropdown"></i>
+          </button>
         </div>
       </div>
-    </UserAgentProvider>
+
+      <ul className={display}>
+        {props.list.map((e, key) => {
+          return (
+            <li className="top-list-item" key={key}>
+              <a href={e.url} className="inherit">
+                {e.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+function DestkopTopbar(props: { list: Array<Item> }) {
+  return (
+    <div className="top">
+      <div className="top-left">
+        <a href="https://github.com/PTFOPlayer/">
+          <i className="fab fa-github"></i>
+        </a>
+      </div>
+      <div className="top-center">
+        <ul className="top-list">
+          {props.list.map((e, key) => {
+            return (
+              <li className="top-list-item" key={key}>
+                <a href={e.url} className="inherit">
+                  {e.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="top-right">
+        <ul className="top-list">
+          <a className="bmc" href="https://www.buymeacoffee.com/WhiskyAKM">
+            <img
+              src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+              alt="Buy Me A Coffee"
+            />
+          </a>
+          <li className="top-list-item">
+            <a
+              className="top-list-item"
+              href="https://ptfoplayer.github.io/technews/#/"
+            >
+              Technews
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,14 +4,13 @@ import "./home.scss";
 import Description from "../../components/home/description/Description";
 import Toolset from "../../components/home/toolset/Toolset";
 import Os from "../../components/home/os/Os";
-import Career from "../../components/home/career/Career";
+
 export default function Home() {
   return (
     <div className="home">
       <Description />
       <Toolset />
       <Os />
-      {/* <Career /> */}
     </div>
   );
 }
