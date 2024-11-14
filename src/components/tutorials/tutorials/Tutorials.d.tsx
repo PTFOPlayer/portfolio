ad4f4bd166fd838ab070c9b00976fca598eafb8f
Patryk <patryktofil03@gmail.com>
2024-11-14 22:20:11 +0100

    Merging files into one

--- src/components/tutorials/tutorials/Tutorials.d.tsx
+++
@@ -1,4 +0,0 @@
-export interface MDPost {
-    post_id: number,
-    post_name: string
-}
--- src/pages/Tutorials/Tutorials.tsx
+++ src/pages/Tutorials/Tutorials.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import "./tutorials.scss"
-import TutorialsComponent from "../../components/tutorials/tutorials/Tutorials";
+import "./tutorials.scss";
+
 export default function Tutorials() {
+  const data = [
+    ["https://www.patryk.tofil.eu/c_system_book/", "kurs systemu w c"],
+  ];
   return (
     <div className="tutoring">
-      <TutorialsComponent />
+      <h2> Courses (in polish): </h2>
+      <ul className="courses_list">
+        {data.map((x) => (
+          <li key={x[1]}>
+            <a href={x[0]} className="inherit">
+              <p>{x[1]}</p>
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
