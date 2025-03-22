eef9dd2cd537f64e45c78c69a2b899ca1ab3eb09
Patryk <patryktofil03@gmail.com>
2025-03-22 09:38:18 +0100

    fixing courses list

--- src/pages/Tutorials/Tutorials.tsx
+++ src/pages/Tutorials/Tutorials.tsx
@@ -4,17 +4,17 @@ import "./tutorials.scss";
 export default function Tutorials() {
   const data = [
     ["https://www.patryk.tofil.eu/c_system_book/", "kurs systemu w c"],
+    ["https://www.patryk.tofil.eu/systemd_book/", "kurs systemd"],
   ];
   return (
     <div className="tutoring">
       <h2> Courses (in polish): </h2>
       <ul className="courses_list">
         {data.map((x) => (
-          <li key={x[1]}>
-            <a href={x[0]} className="inherit">
+          
+            <a key= {x[1]} href={x[0]} className="inherit">
               <p>{x[1]}</p>
             </a>
-          </li>
         ))}
       </ul>
     </div>
